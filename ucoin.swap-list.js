3c4d83c325304530e0642acb992fd4bb04a5e021
Andrius Steponavicius <asteponavicius@kayak.com>
2017-07-19 07:03:18 +0300

    fix tag links

--- ucoin.swap-list.js
+++ ucoin.swap-list.js
@@ -2,7 +2,7 @@
 // @name         uCoin: Swap-list
 // @namespace    https://ucoin.net/
 // @version      0.1.0
-// @description  Update swap-list prices
+// @description  Update swap prices
 // @author       danikas2k2
 // @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/babel-standalone/6.18.2/babel.js
