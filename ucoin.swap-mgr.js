cce5f6c06a8b883c18e903a7e93f4e53ce92ab04
Andrius Steponavicius <asteponavicius@kayak.com>
2017-09-05 15:01:48 +0300

    specific prices added

--- ucoin.swap-mgr.js
+++ ucoin.swap-mgr.js
@@ -1,13 +1,14 @@
 // ==UserScript==
 // @name         uCoin: Swap-Manager
 // @namespace    https://ucoin.net/
-// @version      0.1.1
+// @version      0.1.2
 // @description  Show all prices
 // @author       danikas2k2
 // @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/babel-standalone/6.18.2/babel.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/babel-polyfill/6.16.0/polyfill.js
 // @match        https://*.ucoin.net/swap-mgr/*
+// @match        https://*.ucoin.net/swap-list/*
 // ==/UserScript==
 
 // @formatter:off
@@ -28,12 +29,13 @@ var inline_src = (<><![CDATA[
         function showAllPrices() {
             $('table.swap-coin tr').each((i, tr) => {
                 const $tr = $(tr);
-
                 const $td = $('td.td-cond + td', tr);
-                const myPrice = $td.text();
-                const theyPrice = $tr.data('tooltipPrice');
-                if (myPrice !== theyPrice) {
-                    $td.append(`<br/><span class="gray-11">${theyPrice}</span>`);
+                const myPrice = +$('span.blue-13', $td).text();
+                const prefix = $('span.gray-11:first-child', $td).text();
+                const suffix = $('span.gray-11:last-child', $td).text();
+                const price = +$tr.data('tooltipPrice').replace(prefix, '').replace(suffix, '');
+                if (!isNaN(price) && myPrice !== price) {
+                    $td.append(`<br/><span class="gray-11">${prefix}${price.toFixed(2)}${suffix}</span>`);
                 }
             });
         }
