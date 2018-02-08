9feede4ed52b64b895eb955e38ac23b190c07600
Andrius Steponavicius <asteponavicius@kayak.com>
2018-02-08 08:04:42 +0200

    ucoin: randomDelay time decreased; expand/combine buttons added to swap block for coins

--- ucoin.swap-list.js
+++ ucoin.swap-list.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         uCoin: Swap-list
 // @namespace    https://ucoin.net/
-// @version      0.1.7.3
+// @version      0.1.7.4
 // @description  Update swap prices
 // @author       danikas2k2
 // @icon         data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAIAAACQkWg2AAABuUlEQVQokS2Qv4pfZQBEz8x3d8kWVtEuwVSSIo1d+gTLgM8QSYiQEK0Ci90mvSD2guRNFN/AhMRCMIHdRcE/u79i7zdjcfcBZs7M0RdPn9KhGpeUVHt7ySoJDGGNFmYsTUseNVCxak5HC3NeSALWZG1Y3NZIddslIqDMvULapmOZ1EWXVWnCUIu9LGtZpI+ufnj0zTOgcPj8xcmff4nc+uTmk4cPhikcHr04OT1N4kVuK1dCrWEgzxagw5AKAGlEXlRkzwZSSWLNlGSNpABWEqYcS1lC06KtBUB2xZqJVUgz7IoKrMUBY4laoi0YsDGoDEzBqkJxh9rZiMulFQHAc85NE2Jjga1ie/NDECzdlE9JtEBKmShSHZSw2+1KN8j+wZXpqB4YqYnobndue1aua/vs7Oz1m9+2wOf37plZ5c5ndxGyX719c36+m0GS7n/1tSKVGx9fe/zoyw8O9knR5aW2/+3Wb7//7vc/3m0Ox6e3b1tQ/f3Pv7++foV1/fo1SaRFP/38yw8/vnx/fMxYaFQ2QoeW2YhIgs6m8kBtpdHOVmOMzlgpkCSieIbGeM81GWa0qmU788Lq/6iyH9ZvXMLcAAAAAElFTkSuQmCC
@@ -9,7 +9,7 @@
 // @require      https://cdnjs.cloudflare.com/ajax/libs/babel-standalone/6.18.2/babel.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/babel-polyfill/6.16.0/polyfill.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/notify/0.4.2/notify.min.js
-// @require      https://dev.andriaus.com/ucoin/lib.delay.js?v=0.1.0
+// @require      https://dev.andriaus.com/ucoin/lib.delay.js?v=0.1.1
 // @require      https://dev.andriaus.com/ucoin/lib.prices.js?v=0.1.8
 // @require      https://dev.andriaus.com/ucoin/lib.notify.js?v=0.1.0
 // @require      https://dev.andriaus.com/ucoin/lib.utils.js?v=0.1.0
