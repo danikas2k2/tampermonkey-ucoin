e7c16f4ca433b3de99643b94433a29c54d1b3fe5
Andrius Steponavicius <asteponavicius@kayak.com>
2018-05-10 11:32:29 +0300

    ucoin: added "expand to 5" button

--- ucoin.coin.js
+++ ucoin.coin.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         uCoin: Coin
 // @namespace    https://ucoin.net/
-// @version      0.1.21
+// @version      0.1.22
 // @description  Fix tag links, add publicity toggler, expand/combine swap coins, and update swap prices
 // @author       danikas2k2
 // @icon         data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAIAAACQkWg2AAABuUlEQVQokS2Qv4pfZQBEz8x3d8kWVtEuwVSSIo1d+gTLgM8QSYiQEK0Ci90mvSD2guRNFN/AhMRCMIHdRcE/u79i7zdjcfcBZs7M0RdPn9KhGpeUVHt7ySoJDGGNFmYsTUseNVCxak5HC3NeSALWZG1Y3NZIddslIqDMvULapmOZ1EWXVWnCUIu9LGtZpI+ufnj0zTOgcPj8xcmff4nc+uTmk4cPhikcHr04OT1N4kVuK1dCrWEgzxagw5AKAGlEXlRkzwZSSWLNlGSNpABWEqYcS1lC06KtBUB2xZqJVUgz7IoKrMUBY4laoi0YsDGoDEzBqkJxh9rZiMulFQHAc85NE2Jjga1ie/NDECzdlE9JtEBKmShSHZSw2+1KN8j+wZXpqB4YqYnobndue1aua/vs7Oz1m9+2wOf37plZ5c5ndxGyX719c36+m0GS7n/1tSKVGx9fe/zoyw8O9knR5aW2/+3Wb7//7vc/3m0Ox6e3b1tQ/f3Pv7++foV1/fo1SaRFP/38yw8/vnx/fMxYaFQ2QoeW2YhIgs6m8kBtpdHOVmOMzlgpkCSieIbGeM81GWa0qmU788Lq/6iyH9ZvXMLcAAAAAElFTkSuQmCC
@@ -244,10 +244,18 @@ var inline_src = (<><![CDATA[
                 if (expand.length) {
                     expand.show();
                 } else {
-                    expand = $('<button id="expand" type="button" class="btn--combiners btn-s btn-blue">Expand</button>');
+                    expand = $('<button id="expand" type="button" class="btn--combiners btn-s btn-blue">Ex/All</button>');
                     buttons.append(expand);
-
                     expand.click(() => expandClicked());
+                }
+
+                expand = $('#expand-x5', buttons);
+                if (expand.length) {
+                    expand.show();
+                } else {
+                    expand = $('<button id="expand-x5" type="button" class="btn--combiners btn-s btn-blue">Ex/5</button>');
+                    buttons.append(expand);
+                    expand.click(() => expandClicked(5));
                 }
 
                 expand = $('#expand-x10', buttons);
@@ -256,7 +264,6 @@ var inline_src = (<><![CDATA[
                 } else {
                     expand = $('<button id="expand-x10" type="button" class="btn--combiners btn-s btn-blue">Ex/10</button>');
                     buttons.append(expand);
-
                     expand.click(() => expandClicked(10));
                 }
             }
@@ -319,6 +326,7 @@ var inline_src = (<><![CDATA[
 
             function removeExpandButtons() {
                 $('button#expand', buttons).hide();
+                $('button#expand-x5', buttons).hide();
                 $('button#expand-x10', buttons).hide();
             }
 
