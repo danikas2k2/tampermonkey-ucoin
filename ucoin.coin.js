c9ac3b0face6033949204eb5fff42c26cb8c493b
Andrius Steponavicius <asteponavicius@kayak.com>
2018-06-11 11:05:30 +0300

    ucoin: color buttons for each quality added instead of one general button

--- ucoin.coin.js
+++ ucoin.coin.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         uCoin: Coin
 // @namespace    https://ucoin.net/
-// @version      0.1.25
+// @version      0.1.26
 // @description  Fix tag links, add publicity toggler, expand/combine swap coins, and update swap prices
 // @author       danikas2k2
 // @icon         data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAIAAACQkWg2AAABuUlEQVQokS2Qv4pfZQBEz8x3d8kWVtEuwVSSIo1d+gTLgM8QSYiQEK0Ci90mvSD2guRNFN/AhMRCMIHdRcE/u79i7zdjcfcBZs7M0RdPn9KhGpeUVHt7ySoJDGGNFmYsTUseNVCxak5HC3NeSALWZG1Y3NZIddslIqDMvULapmOZ1EWXVWnCUIu9LGtZpI+ufnj0zTOgcPj8xcmff4nc+uTmk4cPhikcHr04OT1N4kVuK1dCrWEgzxagw5AKAGlEXlRkzwZSSWLNlGSNpABWEqYcS1lC06KtBUB2xZqJVUgz7IoKrMUBY4laoi0YsDGoDEzBqkJxh9rZiMulFQHAc85NE2Jjga1ie/NDECzdlE9JtEBKmShSHZSw2+1KN8j+wZXpqB4YqYnobndue1aua/vs7Oz1m9+2wOf37plZ5c5ndxGyX719c36+m0GS7n/1tSKVGx9fe/zoyw8O9knR5aW2/+3Wb7//7vc/3m0Ox6e3b1tQ/f3Pv7++foV1/fo1SaRFP/38yw8/vnx/fMxYaFQ2QoeW2YhIgs6m8kBtpdHOVmOMzlgpkCSieIbGeM81GWa0qmU788Lq/6iyH9ZvXMLcAAAAAElFTkSuQmCC
@@ -568,15 +568,28 @@ var inline_src = (<><![CDATA[
             if (swapForm.length) {
                 $('head').append(`<style type="text/css">
                     #coin #swap-form .btn-ctrl {
-                    float: right;
-                    margin: 14px 3px 0;
-                    height: 26px;
+                        float: right;
+                        margin: 14px 3px 0;
+                        height: 26px;
                     }
                     #coin #swap-form .btn-ctrl + .btn-ctrl {
-                    margin-right: 0;
+                        margin-right: 0;
                     }
                     #coin #swap-form #swap-qty {
-                    margin-top: 1em;
+                        margin-top: 1em;
+                    }
+                    #coin #swap-block center div.btn-set {
+                        display: flex;
+                        justify-content: space-between;
+                        margin: 0 1em;
+                    }
+                    #coin #swap-block center div.btn-set div {
+                        flex: 0 0 20px;
+                        width: 20px;
+                        height: 20px;
+                        line-height: 20px;
+                        cursor: pointer;
+                        padding: 1px;
                     }
                 </style>`);
 
@@ -604,6 +617,19 @@ var inline_src = (<><![CDATA[
                     _onCoinSwapForm(...args);
                     $(`input[name="condition"][value="${args[1]}"]`, swapForm).prop('checked', true);
                 };
+
+                const addButton = $('#swap-block center button.btn-s.btn-gray');
+                const addButtonSet = $('<div class="btn-set"/>');
+                $('<div class="marked-6">?</div>').click(() => CoinSwapFormOn('','0')).appendTo(addButtonSet);
+                $('<div class="marked-7">G</div>').click(() => CoinSwapFormOn('','6')).appendTo(addButtonSet);
+                $('<div class="marked-8">VG</div>').click(() => CoinSwapFormOn('','5')).appendTo(addButtonSet);
+                $('<div class="marked-9">F</div>').click(() => CoinSwapFormOn('','4')).appendTo(addButtonSet);
+                $('<div class="marked-10">VF</div>').click(() => CoinSwapFormOn('','3')).appendTo(addButtonSet);
+                $('<div class="marked-11">XF</div>').click(() => CoinSwapFormOn('','2')).appendTo(addButtonSet);
+                $('<div class="marked-12">UN</div>').click(() => CoinSwapFormOn('','1')).appendTo(addButtonSet);
+                $('<div class="marked-3">PR</div>').click(() => CoinSwapFormOn('','7')).appendTo(addButtonSet);
+                $('<div class="marked-5">CP</div>').click(() => CoinSwapFormOn('','100')).appendTo(addButtonSet);
+                addButton.replaceWith(addButtonSet);
             }
         }
 
