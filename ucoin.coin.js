24de6d58f6bc32d9995401e685351404edac10e2
Andrius Steponavicius <asteponavicius@kayak.com>
2018-07-18 11:25:31 +0300

    ucoin: added separate price detection for variety in swap

--- ucoin.coin.js
+++ ucoin.coin.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         uCoin: Coin
 // @namespace    https://ucoin.net/
-// @version      0.1.26
+// @version      0.1.27
 // @description  Fix tag links, add publicity toggler, expand/combine swap coins, and update swap prices
 // @author       danikas2k2
 // @icon         data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAIAAACQkWg2AAABuUlEQVQokS2Qv4pfZQBEz8x3d8kWVtEuwVSSIo1d+gTLgM8QSYiQEK0Ci90mvSD2guRNFN/AhMRCMIHdRcE/u79i7zdjcfcBZs7M0RdPn9KhGpeUVHt7ySoJDGGNFmYsTUseNVCxak5HC3NeSALWZG1Y3NZIddslIqDMvULapmOZ1EWXVWnCUIu9LGtZpI+ufnj0zTOgcPj8xcmff4nc+uTmk4cPhikcHr04OT1N4kVuK1dCrWEgzxagw5AKAGlEXlRkzwZSSWLNlGSNpABWEqYcS1lC06KtBUB2xZqJVUgz7IoKrMUBY4laoi0YsDGoDEzBqkJxh9rZiMulFQHAc85NE2Jjga1ie/NDECzdlE9JtEBKmShSHZSw2+1KN8j+wZXpqB4YqYnobndue1aua/vs7Oz1m9+2wOf37plZ5c5ndxGyX719c36+m0GS7n/1tSKVGx9fe/zoyw8O9knR5aW2/+3Wb7//7vc/3m0Ox6e3b1tQ/f3Pv7++foV1/fo1SaRFP/38yw8/vnx/fMxYaFQ2QoeW2YhIgs6m8kBtpdHOVmOMzlgpkCSieIbGeM81GWa0qmU788Lq/6iyH9ZvXMLcAAAAAElFTkSuQmCC
@@ -12,7 +12,7 @@
 // @require      https://dev.andriaus.com/ucoin/lib.delay.js?v=0.1.1
 // @require      https://dev.andriaus.com/ucoin/lib.links.js?v=0.1.1
 // @require      https://dev.andriaus.com/ucoin/lib.notify.js?v=0.1.0
-// @require      https://dev.andriaus.com/ucoin/lib.prices.js?v=0.1.8
+// @require      https://dev.andriaus.com/ucoin/lib.prices.js?v=0.1.9
 // @require      https://dev.andriaus.com/ucoin/lib.utils.js?v=0.1.0
 // @match        https://*.ucoin.net/coin/*
 // ==/UserScript==
@@ -535,6 +535,7 @@ var inline_src = (<><![CDATA[
                 const name    = sp($('th:contains("Denomination") + td', coin).text());
                 const subject = sp($('th:contains("Subject") + td', coin).text());
                 const price   = sp($('a[href="#price"] span', coin).text());
+                const varieties = $(`+h3 +table td`, $('#variety', coin));
 
                 let queue = $.when();
 
@@ -543,10 +544,12 @@ var inline_src = (<><![CDATA[
                     const onclick = $a.attr('onclick');
                     const [uniq, usid, cond, pp, info, vid, qty] = m;
 
+                    const varietyPrice = vid && sp($(`a[href*="vid=${vid}#"]`, varieties).text()).replace(/[^0-9.]/g, '') || price;
+
                     const year = sp($('span.left.gray-13', $a).text());
                     const q    = sp($('span.left.dgray-11', $a).text());
 
-                    const p = getPrice(config, country, name, subject, year, q, info, price);
+                    const p = getPrice(config, country, name, subject, year, q, info, varietyPrice);
                     if (p === false || p === pp || p === `${pp}.00`) {
                         return;
                     }
