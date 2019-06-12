af335456ed4a89711e999c13a55ca37337b7bfe8
Andrius Steponavičius <asteponavicius@kayak.com>
2019-06-12 13:40:20 +0300

    notification functions made async

--- src/lib/notify.ts
+++ src/lib/notify.ts
@@ -1,11 +1,11 @@
 // @ts-ignore
-import OK from '../../images/success.svg';
-// @ts-ignore
 import ERROR from '../../images/error.svg';
 // @ts-ignore
 import INFO from '../../images/info.svg';
 // @ts-ignore
 import WARNING from '../../images/minus.svg';
+// @ts-ignore
+import OK from '../../images/success.svg';
 
 function notify(title: string, body?: string, icon?: string): void {
     const options: NotificationOptions = {};
@@ -18,32 +18,31 @@ function notify(title: string, body?: string, icon?: string): void {
     new Notification(title, options);
 }
 
-function request(title: string, body?: string, icon?: string): void {
-    if (!("Notification" in window)) {
+async function request(title: string, body?: string, icon?: string): Promise<void> {
+    if (!('Notification' in window)) {
         alert(title);
     } else if (Notification.permission === 'granted') {
         notify(title, body, icon);
     } else if (Notification.permission !== 'denied') {
-        Notification.requestPermission().then(permission => {
-            if (permission === 'granted') {
-                notify(title, body, icon);
-            }
-        });
+        const permission = await Notification.requestPermission();
+        if (permission === 'granted') {
+            notify(title, body, icon);
+        }
     }
 }
 
-export function info(title: string, body?: string): void {
-    request(title, body, INFO);
+export async function info(title: string, body?: string): Promise<void> {
+    await request(title, body, INFO);
 }
 
-export function err(title: string, body?: string): void {
-    request(title, body, ERROR);
+export async function err(title: string, body?: string): Promise<void> {
+    await request(title, body, ERROR);
 }
 
-export function warn(title: string, body?: string): void {
-    request(title, body, WARNING);
+export async function warn(title: string, body?: string): Promise<void> {
+    await request(title, body, WARNING);
 }
 
-export function ok(title: string, body?: string): void {
-    request(title, body, OK);
+export async function ok(title: string, body?: string): Promise<void> {
+    await request(title, body, OK);
 }
