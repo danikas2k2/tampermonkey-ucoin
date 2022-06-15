ec407caa1d97984ac65c22229a4ec55b4bfae6bb
Andrius Steponavicius <danikas2k2@gmail.com>
2022-06-15 11:21:26 +0300

    - jest setup added; - first test added;

---
+++ src/lib/delay.test.ts
@@ -0,0 +1,139 @@
+import { waitFor } from '@testing-library/dom';
+import { decorateDelay, delay, randomDelay } from './delay';
+
+describe('delay', () => {
+    const mockComplete = jest.fn();
+    const mockDecorator = jest.fn();
+
+    beforeEach(() => {
+        jest.useFakeTimers();
+    });
+
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    afterAll(() => {
+        jest.useRealTimers();
+    });
+
+    it('should delay for given time', async () => {
+        delay(1000, mockDecorator).finally(mockComplete);
+        jest.advanceTimersByTime(500);
+        await waitFor(() => expect(mockComplete).not.toHaveBeenCalled());
+        jest.advanceTimersByTime(1000);
+        await waitFor(() => expect(mockComplete).toHaveBeenCalled());
+        await waitFor(() => expect(mockDecorator).not.toHaveBeenCalled());
+    });
+
+    it('call decorator if delay longer than 1 second', async () => {
+        delay(1500, mockDecorator).finally(mockComplete);
+        await waitFor(() => expect(mockDecorator).toHaveBeenCalledWith(1500, 1000));
+        await waitFor(() => expect(mockComplete).not.toHaveBeenCalled());
+        jest.advanceTimersByTime(1500);
+        await waitFor(() => expect(mockComplete).toHaveBeenCalled());
+    });
+
+    it('do not call decorator if delay longer shorted than 2 second', async () => {
+        delay(1500, mockDecorator, 2000).finally(mockComplete);
+        await waitFor(() => expect(mockComplete).not.toHaveBeenCalled());
+        jest.advanceTimersByTime(1500);
+        await waitFor(() => expect(mockComplete).not.toHaveBeenCalled());
+        await waitFor(() => expect(mockDecorator).not.toHaveBeenCalled());
+    });
+
+    it('call decorator if delay longer than 2 second', async () => {
+        delay(2500, mockDecorator, 2000).finally(mockComplete);
+        await waitFor(() => expect(mockDecorator).toHaveBeenCalledWith(2500, 2000));
+        await waitFor(() => expect(mockComplete).not.toHaveBeenCalled());
+        jest.advanceTimersByTime(2500);
+        await waitFor(() => expect(mockComplete).not.toHaveBeenCalled());
+    });
+});
+
+jest.spyOn(Math, 'random').mockReturnValue(0.5);
+
+describe('randomDelay', () => {
+    const mockComplete = jest.fn();
+    const mockDecorator = jest.fn();
+
+    beforeEach(() => {
+        jest.useFakeTimers();
+    });
+
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    afterAll(() => {
+        jest.useRealTimers();
+    });
+
+    it('should delay for random time, starting from default minimum', async () => {
+        randomDelay(1000, undefined, mockDecorator).finally(mockComplete);
+        await waitFor(() => expect(mockDecorator).toHaveBeenCalledWith(2000, 1000));
+        await waitFor(() => expect(mockComplete).not.toHaveBeenCalled());
+        jest.advanceTimersByTime(1000);
+        await waitFor(() => expect(mockComplete).not.toHaveBeenCalled());
+        jest.advanceTimersByTime(1500);
+        await waitFor(() => expect(mockComplete).not.toHaveBeenCalled());
+        jest.advanceTimersByTime(2000);
+        await waitFor(() => expect(mockComplete).toHaveBeenCalled());
+    });
+
+    it('should delay for random time, starting from custom minimum', async () => {
+        randomDelay(1000, 1000, mockDecorator).finally(mockComplete);
+        await waitFor(() => expect(mockDecorator).toHaveBeenCalledWith(1500, 1000));
+        await waitFor(() => expect(mockComplete).not.toHaveBeenCalled());
+        jest.advanceTimersByTime(1000);
+        await waitFor(() => expect(mockComplete).not.toHaveBeenCalled());
+        jest.advanceTimersByTime(1500);
+        await waitFor(() => expect(mockComplete).toHaveBeenCalled());
+    });
+
+    it('should delay for different random time, starting from default minimum', async () => {
+        (Math.random as jest.Mock).mockReturnValue(0.25);
+        randomDelay(1000, undefined, mockDecorator).finally(mockComplete);
+        await waitFor(() => expect(mockDecorator).toHaveBeenCalledWith(1750, 1000));
+        await waitFor(() => expect(mockComplete).not.toHaveBeenCalled());
+        jest.advanceTimersByTime(1000);
+        await waitFor(() => expect(mockComplete).not.toHaveBeenCalled());
+        jest.advanceTimersByTime(1500);
+        await waitFor(() => expect(mockComplete).not.toHaveBeenCalled());
+        jest.advanceTimersByTime(1750);
+        await waitFor(() => expect(mockComplete).toHaveBeenCalled());
+    });
+
+    it('call decorator if delay longer than 1 second', async () => {
+        randomDelay(1000, undefined, mockDecorator).finally(mockComplete);
+        await waitFor(() => expect(mockDecorator).toHaveBeenCalledWith(1750, 1000));
+        await waitFor(() => expect(mockComplete).not.toHaveBeenCalled());
+        jest.advanceTimersByTime(1500);
+        await waitFor(() => expect(mockComplete).toHaveBeenCalled());
+    });
+
+    it('do not call decorator if delay longer shorted than 2 second', async () => {
+        randomDelay(1000, undefined, mockDecorator, 2000).finally(mockComplete);
+        await waitFor(() => expect(mockComplete).not.toHaveBeenCalled());
+        jest.advanceTimersByTime(1500);
+        await waitFor(() => expect(mockComplete).not.toHaveBeenCalled());
+        await waitFor(() => expect(mockDecorator).not.toHaveBeenCalled());
+    });
+
+    it('call decorator if delay longer than 2 second', async () => {
+        randomDelay(2500, undefined, mockDecorator, 2000).finally(mockComplete);
+        await waitFor(() => expect(mockDecorator).toHaveBeenCalledWith(2125, 2000));
+        await waitFor(() => expect(mockComplete).not.toHaveBeenCalled());
+        jest.advanceTimersByTime(2500);
+        await waitFor(() => expect(mockComplete).not.toHaveBeenCalled());
+    });
+});
+
+describe('decorateDelay', () => {
+    it('add decoration element to document', async () => {
+        decorateDelay(1000);
+        const decorator = document.getElementById('display-delay');
+        expect(decorator).toBeInTheDocument();
+        expect(decorator!.firstChild).toHaveStyle('animation-duration: 1000ms');
+    });
+});
--- src/lib/delay.ts
+++ src/lib/delay.ts
@@ -1,20 +1,38 @@
-export async function delay(time: number): Promise<void> {
-    console.debug(`DELAY FOR ${time} MS`);
-    if (time > 1000) {
-        const id = 'display-delay';
-        let el = document.getElementById(id);
-        if (!el) {
-            document.body.insertAdjacentHTML('beforeend', `<div id='${id}'/>`);
-            el = document.getElementById(id);
-        }
-        if (el) {
-            el.innerHTML = `<div style='animation-duration: ${time}ms'/>`;
-        }
+type DelayDecorator = (time: number, decorateFrom: number) => void;
+
+export function decorateDelay(time: number) {
+    const id = 'display-delay';
+    let el = document.getElementById(id);
+    if (!el) {
+        document.body.insertAdjacentHTML('beforeend', `<div id='${id}'/>`);
+        el = document.getElementById(id);
     }
-    return await new Promise((resolve) => setTimeout(() => resolve(), time));
+    if (el) {
+        el.innerHTML = `<div style='animation-duration: ${time}ms'/>`;
+    }
 }
 
-export async function randomDelay(rndDelay = 1500, minDelay = 1500): Promise<void> {
-    const time = Math.round(minDelay + Math.random() * rndDelay);
-    return await delay(time);
+const DECORATE_FROM_DELAY = 1000;
+
+export async function delay(
+    time: number,
+    decorator: DelayDecorator = decorateDelay,
+    decorateFrom = DECORATE_FROM_DELAY
+): Promise<void> {
+    if (time > decorateFrom) {
+        decorator(time, decorateFrom);
+    }
+    return new Promise((resolve) => setTimeout(() => resolve(), time));
+}
+
+const DEFAULT_RANDOM_DELAY = 1500;
+const MIN_RANDOM_DELAY = 1500;
+
+export async function randomDelay(
+    rndDelay = DEFAULT_RANDOM_DELAY,
+    minDelay = MIN_RANDOM_DELAY,
+    decorator: DelayDecorator = decorateDelay,
+    decorateFrom = DECORATE_FROM_DELAY
+): Promise<void> {
+    return delay(Math.round(minDelay + Math.random() * rndDelay), decorator, decorateFrom);
 }
