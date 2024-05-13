ddb8121cca66d030bf5650bc2358ef0e15621460
Andrius Steponavicius <danikas2k2@gmail.com>
2024-05-13 10:38:53 +0300

    1.14.0

--- dist/ucoin.user.js
+++ dist/ucoin.user.js
@@ -2,7 +2,7 @@
 // ==UserScript==
 // @name         collector :: ucoin.net
 // @namespace    https://ucoin.net/
-// @version      1.13.0
+// @version      1.14.0
 // @author       danikas2k2
 // @icon         https://i.ibb.co/Tc6q9x3/apple-touch-icon-152.png
 // @downloadURL  https://raw.githubusercontent.com/danikas2k2/tampermonkey-ucoin/master/dist/ucoin.user.js
